1c8533a7c5d5bee101249f6c4f6d4873256bc2da
wpmuguru <wpmuguru@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-20 02:27:47 +0000

    update for WP 3.0
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-mu-theme-stats/trunk@264734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ra-theme-stats.php
+++ ra-theme-stats.php
@@ -1,12 +1,13 @@
 <?php
 /*
-Plugin Name: Wordpress MU Theme Stats
+Plugin Name: Wordpress Network Theme Stats
 Plugin URI: http://wpmututorials.com/
 Description: Adds submenu to see theme stats, shows themes by user and most popular themes.
-Version: 2.8
-Author: Ron Rennick (original plugin contributions by Phillip Studinski)
+Version: 2.8.1
+Author: Ron Rennick
 Author URI: http://ronandandrea.com/
- 
+
+(original plugin contributions by Phillip Studinski)
 */
 /* Copyright:	(C) 2009 Ron Rennick, All rights reserved.  
 
@@ -24,102 +25,104 @@ Author URI: http://ronandandrea.com/
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-function ra_theme_stats_add_page() {
-	// Add a submenu
-	if(is_site_admin()) {
-		add_submenu_page('wpmu-admin.php', 'Theme Stats', 'Theme Stats', 0, basename(__FILE__), 'ra_theme_stats_page');
-		add_action('admin_head','ra_add_show_hide_css');
+class RA_Theme_Stats {
+	function RA_Theme_Stats() {
+		add_action( 'admin_menu', array( &$this, 'add_page' ) );
 	}
-}
-add_action('admin_menu', 'ra_theme_stats_add_page');
+	function add_page() {
+		if( is_super_admin() ) {
+			add_submenu_page('ms-admin.php', 'Theme Stats', 'Theme Stats', 'manage_network_themes', 'ra_theme_stats', array( &$this, 'admin_page' ) );
+			if( $_GET['page'] == 'ra_theme_stats' )
+				add_action( 'admin_head', array( &$this, 'show_hide_css' ) );
+		}
+	}
 
-function ra_theme_stats_page() { 
-	if( !is_site_admin() ) {
-		die( 'You don\'t have permissions to use this page.' );
-	} ?>
-<div class=wrap>
-      <h2>Theme Statistics</h2>
-<?php
-	global $wpdb, $current_site;
-	$blogs  = $wpdb->get_results("SELECT blog_id, domain, path FROM $wpdb->blogs " .
-		"WHERE site_id = {$current_site->id} ORDER BY domain ASC");
-	$blogtheme = array();
-	if ($blogs) {
-		foreach ($blogs as $blog) {
-			$blogOptionsTable  = "{$wpdb->base_prefix}{$blog->blog_id}_options";
-			$blogtemplate = $wpdb->get_var("SELECT option_value FROM $blogOptionsTable WHERE option_name = 'template'");
-			if($blogtemplate)
-				$blogtheme[$blog->blog_id] = $blogtemplate;
-		}
-	}
-	$themeinfo = array();
-	$themeblogs = array();
-	// do stats
-	if($blogs) {
-		foreach ($blogs as $blog) {
-			if( !array_key_exists( $blogtheme[$blog->blog_id], $themeinfo ) ) {
-				$themeinfo[$blogtheme[$blog->blog_id]] = 1;
-				$themeblogs[$blogtheme[$blog->blog_id]] = array();
-				$themeblogs[$blogtheme[$blog->blog_id]][0] = $blog;
-			} else {
-				$themeblogs[$blogtheme[$blog->blog_id]][$themeinfo[$blogtheme[$blog->blog_id]]] = $blog;
-				$themeinfo[$blogtheme[$blog->blog_id]]++;
-			}
-		}
-	}
-	arsort($themeinfo);
-	// show stats
-	echo '<ul>';
-	foreach( $themeinfo as $themename => $themecount ) {
-		echo '<li>';
-		ra_show_hide_begin($themename, "$themename ($themecount)", '','ul'); ?>
-<?php
-		foreach($themeblogs[$themename] as $bloginfo) {
-			$url = "http://" . $bloginfo->domain . $bloginfo->path;
-			if($bloginfo->path == '/') {
-				$domain = explode('.',$bloginfo->domain);
-				$blogname = $domain[0];
-			} else {
-				$blogname = substr($bloginfo->path, 1, -1);
-			}
-			echo '<li><a href="' . $url . '">' . $blogname .  '</a> - <a href ="' . $url . "wp-admin/" . '">Backend</a></li>';
+	function admin_page() {
+		if( !current_user_can( 'manage_network_themes' ) ) {
+			wp_die( 'You don\'t have permissions to use this page.' );
 		} ?>
-<?php
-		ra_show_hide_end('ul');
-		echo '<br /></li>';
-	} ?>
-</ul>
-<script type="text/javascript"><!--
-	function ra_show(id, newclass)
-	{
-	  var el = document.getElementById(id);
-	  if(el) {
-	    if(newclass) {
-		if(el.className==newclass) el.className="ra-hide"; 
-		else el.className=newclass;
-	    } else {
-		if(el.className=="") el.className="ra-hide";
-		else el.className="";
-	    }
-	  }
+	<div class=wrap>
+	      <h2>Theme Statistics</h2>
+	<?php
+		global $wpdb, $current_site;
+		$blogs  = $wpdb->get_results("SELECT blog_id, domain, path FROM $wpdb->blogs " .
+			"WHERE site_id = {$current_site->id} ORDER BY domain ASC");
+		$blogtheme = array();
+		if ($blogs) {
+			foreach ($blogs as $blog) {
+				if( ( $blogtemplate = get_blog_option( $blog->blog_id, 'template' ) ) )
+					$blogtheme[$blog->blog_id] = $blogtemplate;
+			}
+		}
+		$themeinfo = array();
+		$themeblogs = array();
+		// do stats
+		if($blogs) {
+			foreach ($blogs as $blog) {
+				if( !array_key_exists( $blogtheme[$blog->blog_id], $themeinfo ) ) {
+					$themeinfo[$blogtheme[$blog->blog_id]] = 1;
+					$themeblogs[$blogtheme[$blog->blog_id]] = array();
+					$themeblogs[$blogtheme[$blog->blog_id]][0] = $blog;
+				} else {
+					$themeblogs[$blogtheme[$blog->blog_id]][$themeinfo[$blogtheme[$blog->blog_id]]] = $blog;
+					$themeinfo[$blogtheme[$blog->blog_id]]++;
+				}
+			}
+		}
+		arsort($themeinfo);
+		// show stats
+		echo '<ul>';
+		foreach( $themeinfo as $themename => $themecount ) {
+			echo '<li>';
+			$this->show_hide_begin($themename, "$themename ($themecount)", '','ul'); ?>
+	<?php
+			foreach($themeblogs[$themename] as $bloginfo) {
+				$url = "http://" . $bloginfo->domain . $bloginfo->path;
+				if($bloginfo->path == '/') {
+					$domain = explode('.',$bloginfo->domain);
+					$blogname = $domain[0];
+				} else {
+					$blogname = substr($bloginfo->path, 1, -1);
+				}
+				echo '<li><a href="' . $url . '">' . $blogname .  '</a> - <a href ="' . $url . "wp-admin/" . '">Backend</a></li>';
+			} ?>
+	<?php
+			$this->show_hide_end('ul');
+			echo '<br /></li>';
+		} ?>
+	</ul>
+	<script type="text/javascript"><!--
+		function ra_show(id, newclass)
+		{
+		  var el = document.getElementById(id);
+		  if(el) {
+		    if(newclass) {
+			if(el.className==newclass) el.className="ra-hide";
+			else el.className=newclass;
+		    } else {
+			if(el.className=="") el.className="ra-hide";
+			else el.className="";
+		    }
+		  }
+		}
+	//--></script>
+	</div><?php
 	}
-//--></script>
-</div><?php
-}
 
-function ra_add_show_hide_css() { ?>
-<style type="text/css">.ra-hide { display:none; }</style> 
-<?php }
+	function show_hide_css() { ?>
+	<style type="text/css">.ra-hide { display:none; }</style>
+	<?php }
 
-function ra_show_hide_begin($HTMLid, $linktext = 'Expand/Collapse', $CSSclass = '', $tag = 'div') {
-	$q = "'";
-	if($HTMLid && $linktext) {
-		echo '<a href="javascript:void(0)" onclick="ra_show('.$q.$HTMLid.$q.','.
-		$q.$CSSclass.$q.')">'.$linktext.'</a>';
-		echo '<'.$tag.' id="'.$HTMLid.'" class="ra-hide">';
+	function show_hide_begin($HTMLid, $linktext = 'Expand/Collapse', $CSSclass = '', $tag = 'div') {
+		$q = "'";
+		if($HTMLid && $linktext) {
+			echo '<a href="javascript:void(0)" onclick="ra_show('.$q.$HTMLid.$q.','.
+			$q.$CSSclass.$q.')">'.$linktext.'</a>';
+			echo '<'.$tag.' id="'.$HTMLid.'" class="ra-hide">';
+		}
+	}
+	function show_hide_end($tag = 'div') {
+		echo '</'.$tag.'>';
 	}
 }
-function ra_show_hide_end($tag = 'div') { 
-	echo '</'.$tag.'>';
-}
-?>
+$ra_theme_stats = new RA_Theme_Stats();
--- readme.txt
+++ readme.txt
@@ -1,21 +1,21 @@
 === Wordpress MU Theme Stats ===
 Contributors: wpmuguru
-Tags: theme, stats, statistics, wpmu 
-Requires at least: 1.2
-Tested up to: 2.8.2
+Tags: theme, stats, statistics, multisite
+Requires at least: 3.0
+Tested up to: 3.0
 Stable tag: trunk
 
 Adds submenu to see theme stats, shows themes by user and most popular themes.
 
 == Description ==
 
-Adds a Theme Stats option to the site admin menu. The admin panel lists all the themes in use site-wide. At the top of the list will be the most popular theme and a number in brackets of how many are in use. Click on the name, and a drop-down list will appear of all blogs using that theme, with a link to each blog's home and dashboard.
+Adds a Theme Stats option to the super admin menu. The admin panel lists all the themes in use site-wide. At the top of the list will be the most popular theme and a number in brackets of how many are in use. Click on the name, and a drop-down list will appear of all blogs using that theme, with a link to each blog's home and dashboard.
 
 This plugin goes in the mu-plugins folder. This is an updated version of the theme stats (for subdomain installs) plugin we released in 2007. This version is designed for either subdomain or subdirectory installs.
 
 Support can be obtained through:
 
-[Wordpress MU Forum](http://mu.wordpress.org/forums/)
+[Wordpress Forum](http://wordpress.org/support/)
 
 [Twitter](http://twitter.com/wpmuguru)
 
@@ -28,6 +28,9 @@ Support can be obtained through:
 1. Upload `ra-theme-stats.php` to the `/wp-content/mu-plugins/` directory
 
 == Changelog ==
+
+= 2.8.1 =
+* Update for WordPress 3.0
 
 = 2.8 =
 * Original version.
